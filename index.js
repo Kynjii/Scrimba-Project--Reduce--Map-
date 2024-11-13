367f65acc2ba2ea637f58a21f946433266077b9d
Dean B <deantburrows88@gmail.com>
2024-11-13 17:27:51 +0100

    refactor based on solution
    While my approach worked, I needed to include Object Destructuring so I included it and refactored to follow the solution presented in the course.

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
 import {propertyForSaleArr} from '/properties/propertyForSaleArr.js'
 import {placeholderPropertyObj} from '/properties/placeholderPropertyObj.js'
 
-
-function getPropertyHtml(propertyArr){
-    if (!propertyArr){
-        const totalSize = placeholderPropertyObj.roomsM2.reduce((sum, room) => sum + room, 0)
-        return `<section class="card">
-                    <img src="${placeholderPropertyObj.image}">
-                    <div class="card-right">
-                        <h2>${placeholderPropertyObj.propertyLocation}</h2>
-                        <h3>${placeholderPropertyObj.priceGBP}</h3>
-                        <p>${placeholderPropertyObj.comment}</p>
-                        <h3>${totalSize}m&sup2;</h3>
-                    </div>
-                </section>`
-    }
-    else {
-        const propertiesArr = propertyForSaleArr.map(function(property){
-            const totalSize = property.roomsM2.reduce((sum, room) => sum + room, 0)            
+function getPropertyHtml(propertyArr = [placeholderPropertyObj]){
+        return propertyArr.map(property=> {
+            const {
+                propertyLocation,
+                priceGBP,
+                roomsM2,
+                comment,
+                image
+            } = property
+            const totalSize = roomsM2.reduce((a, b) => a + b)            
             return `<section class="card">
-                        <img src="${property.image}">
+                        <img src="${image}">
                         <div class="card-right">
-                            <h2>${property.propertyLocation}</h2>
-                            <h3>${property.priceGBP}</h3>
-                            <p>${property.comment}</p>
+                            <h2>${propertyLocation}</h2>
+                            <h3>${priceGBP}</h3>
+                            <p>${comment}</p>
                             <h3>${totalSize}m&sup2;</h3>
                         </div>
                     </section> `
-        }).join('')
-        return propertiesArr
-    }      
+        }).join('')        
 }
 
 document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
-
-
-
